d6091d9bac8c8e05c9e9b9af4521277e73dc5383
Terry Biberdorf <terry.biberdorf@symcom.com>
2013-12-11 12:01:42 -0700

    matched ProcessExport.c file

--- Sources/ProcessorExpert.c
+++ Sources/ProcessorExpert.c
@@ -37,6 +37,7 @@
 #include "ADC0.h"
 #include "SPI2.h"
 #include "UTIL1.h"
+#include "FRTOS1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -51,6 +52,19 @@
 extern void TestApp_Init(void);
 extern void TestApp_Task(void);
 
+/* User includes (#include below this line is not maintained by Processor Expert) */
+static portTASK_FUNCTION(Task1, pvParameters) 
+{
+	(void)pvParameters; /* ignore unused parameter */
+	
+	  /* For example: for(;;) { } */
+	  (void)TestApp_Init(); /* Initialize the USB Test Application */
+	  for(;;){
+		  TestApp_Task();
+	  }
+	  
+}
+
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
@@ -62,11 +76,9 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   /* Write your code here */
-  /* For example: for(;;) { } */
-  (void)TestApp_Init(); /* Initialize the USB Test Application */
-  for(;;){
-	  TestApp_Task();
-  }
+  FRTOS1_xTaskCreate(Task1, (signed portCHAR *)"Task1", configMINIMAL_STACK_SIZE*3, NULL, configMAX_PRIORITIES-2, NULL);
+  FRTOS1_vTaskStartScheduler();
+
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
   #ifdef PEX_RTOS_START
